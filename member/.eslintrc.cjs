372817af2eee2d98ec8a6bb66f14514d0b88cc54
wowissu <bbgbbg0123@gmail.com>
2022-06-22 17:19:18 +0800

    chore: eslint as package

--- member/.eslintrc.cjs
+++ member/.eslintrc.cjs
@@ -6,54 +6,11 @@ const path = require('path');
 module.exports = {
   root: true,
 
-  globals: {
-    ga: true, // Google Analytics
-    cordova: true,
-    __statics: true,
-    process: true
-  },
-
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    project: path.resolve(__dirname, './tsconfig.json'),
-    extraFileExtensions: ['.vue']
-    // createDefaultProgram: true
-  },
-
-  // Rules order is important, please avoid shuffling them
   extends: [
-    // Base ESLint recommended rules
-    'eslint:recommended',
-    '@wowissu/eslint-config/common',
-    '@wowissu/eslint-config/file-progress',
-    // '@wowissu/eslint-config/filenames',
-    '@wowissu/eslint-config/unused-imports'
+    '@pork-buns/eslint-config'
   ],
 
-  overrides: [
-    {
-      files: ['*.js'],
-      env: {
-        node: true,
-        es6: true
-      },
-      extends: [
-        '@wowissu/eslint-config/@babel'
-      ]
-    },
-    {
-      files: ['*.ts', '*.tsx', '*.vue'],
-      env: {
-        browser: true
-      },
-      extends: [
-        '@wowissu/eslint-config/ts',
-        '@wowissu/eslint-config/ts/type-checking',
-        '@wowissu/eslint-config/vue/vue3'
-      ],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    }
-  ]
+  parserOptions: {
+    project: path.resolve(__dirname, 'tsconfig.json')
+  }
 };
